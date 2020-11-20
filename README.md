4ecad52e69d5215c43bcfe36f70f8d2af44d89ca
Ymir Egilson <ymir.egilson@gmail.com>
2020-11-20 22:37:06 +0100

    Updated readme with gif and link to demo

--- README.md
+++ README.md
@@ -1,16 +1,5 @@
 # Simple React Todo App
 
-[Live demo](https://react-todo-ymirke.herokuapp.com/)
+[Link to demo](https://react-functional-components.netlify.app/)
 
-
-![Image of todo app](https://github.com/Ymirke/react-todo/blob/master/react-todo2.png)
-
-## About
-Built with functional components and hooks. 
-Class-components branch for class implementation.
-
-### Run with:
-```console
-> npm install 
-> npm start
-```
+![Angular Todo Demo](https://github.com/Ymirke/todo-app-in-web-frameworks/blob/react-functional-components/react-functional.gif)
---
+++ build/precache-manifest.df64548964d1a769ac60e808debeba4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06637bba68629355d7daaae5756260d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c974374917e8a33fbfd8",
+    "url": "/static/css/main.ac50338f.chunk.css"
+  },
+  {
+    "revision": "7275aff5b9c27f5dcdae",
+    "url": "/static/js/2.fbb0c066.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.fbb0c066.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c974374917e8a33fbfd8",
+    "url": "/static/js/main.b3faa8bd.chunk.js"
+  },
+  {
+    "revision": "252c54e5682a184ebd9c",
+    "url": "/static/js/runtime-main.19184806.js"
+  }
+]);
